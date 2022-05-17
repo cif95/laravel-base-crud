f25547c217bf5b3533bc50b6bfd9e87b0e94a7fb
cif95 <vivianacifarelli9@gmail.com>
2022-05-17 17:22:14 +0200

    implemented config file in seeder

--- database/seeds/ComicsTableSeeder.php
+++ database/seeds/ComicsTableSeeder.php
@@ -12,17 +12,39 @@ class ComicsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run(Faker $faker)
-    {
-        for ($i=0; $i < 100 ; $i++) { 
+
+    // Seeding with faker php
+
+
+    // public function run(Faker $faker)
+    // {
+    //     for ($i=0; $i < 100 ; $i++) { 
+    //         $newComic = new Comic();
+    //         $newComic->title = $faker->words(3, true);
+    //         $newComic->description = $faker->paragraph();
+    //         $newComic->thumb = 'https://source.unsplash.com/random/800x800/?img=' . $faker->randomNumber(3, true);
+    //         $newComic->price = $faker->randomFloat(2, 10, 100);
+    //         $newComic->series = $faker->words(3, true);
+    //         $newComic->sale_date = $faker->dateTimeBetween('-2 years', '+1 year');
+    //         $newComic->type = $faker->words(3, true);
+    //         $newComic->save();
+    //     }
+    // }
+
+    
+    // Seeding with a config file
+
+    public function run() {
+        $comics = config('comics');
+        foreach($comics as $comic) {
             $newComic = new Comic();
-            $newComic->title = $faker->words(3, true);
-            $newComic->description = $faker->paragraph();
-            $newComic->thumb = 'https://source.unsplash.com/random/800x800/?img=' . $faker->randomNumber(3, true);
-            $newComic->price = $faker->randomFloat(2, 10, 100);
-            $newComic->series = $faker->words(3, true);
-            $newComic->sale_date = $faker->dateTimeBetween('-2 years', '+1 year');
-            $newComic->type = $faker->words(3, true);
+            $newComic->title = $comic['title'];
+            $newComic->description = $comic['description'];
+            $newComic->thumb = $comic['thumb'];
+            $newComic->price = floatval($comic['price']);
+            $newComic->series = $comic['series'];
+            $newComic->sale_date = $comic['sale_date'];
+            $newComic->type = $comic['type'];
             $newComic->save();
         }
     }
